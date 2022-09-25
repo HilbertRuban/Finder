f0603d9fe872ff74bbd477a89b45d12d53e6233e
Hilbert <hilbertruban53@gmail.com>
2022-09-26 01:22:47 +0530

    added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-import { useState } from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed} from './components'
 
-function App() {
+const App = () => (
+   <BrowserRouter>
+    <Box sx={{ backgroundColor: '#000' }}>
+      <Navbar />
+      <Routes>
+        <Route  path="/" exact element={<Feed />}/>
+        <Route path="/video/:id" element={<VideoDetail />}/>
+        <Route path="/channel/:id" element={<ChannelDetail />}/>
+        <Route path="/search/:searchTerm" element={<SearchFeed />}/>
+      </Routes>
+    </Box>
+   </BrowserRouter>
+);
 
-  return (
-    <div className="App">
-      Finder app
-    </div>
-  )
-}
-
-export default App
+export default App;
