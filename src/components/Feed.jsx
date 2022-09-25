cb9d2a1e0bfad69b02d2b76941d9bcaa5de5d1ec
Hilbert <hilbertruban53@gmail.com>
2022-09-26 02:16:29 +0530

    initialized side bar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { Stack, Box, Typography } from "@mui/material";
+import SideBar from "./SideBar";
 
 const Feed = () => {
   return (
-    <div>Feed</div>
-  )
-}
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sm: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <SideBar />
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          Copyright 2022
+        </Typography>
+      </Box>
+    </Stack>
+  );
+};
 
-export default Feed
+export default Feed;
---
+++ src/components/SideBar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Stack } from "@mui/material";
+import { categories } from "../utils/constants";
+
+const SideBar = () => (
+  <Stack
+    direction="row"
+    sx={{
+      overflow: "auto",
+      height: { sx: "auto", md: "95%" },
+      flexDirection: { md: "column" },
+    }}
+  >
+    {categories.map((category) => {
+      {
+        /* const icon = category.icon.replaceAll('"', ""); */
+      }
+      return (
+        <button>
+          <span>{category.icon}</span>
+          <span>{category.name}</span>
+        </button>
+      );
+    })}
+  </Stack>
+);
+
+export default SideBar;
