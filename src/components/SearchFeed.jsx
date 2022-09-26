810e7ea1d078688c96a8751e7276951f967e9181
Hilbert <hilbertruban53@gmail.com>
2022-09-27 01:56:38 +0530

    added search bar functionality

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Box, Typography } from "@mui/material";
+import { useParams } from "react-router-dom";
+
+import { fetchApi } from "../utils/FetchApi";
+import { Videos } from "./";
 
 const SearchFeed = () => {
-  return (
-    <div>SearchFeed</div>
-  )
-}
+  const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParams();
 
-export default SearchFeed
+  useEffect(() => {
+    fetchApi(`search?part=snippet&q=${searchTerm}`).then((data) =>
+      setVideos(data.items)
+    );
+  }, [searchTerm]);
+
+  return (
+    <Box
+      p={2}
+      sx={{
+        overflow: "auto",
+        height: "90vh",
+        ml:'120px'
+      }}
+    >
+      <Typography
+        variant="h4"
+        fontWeight="bold"
+        mb={2}
+        sx={{
+          color: "#fff",
+        }}
+      >
+        Search Results for: <span style={{ color: "#F31503" }}>{searchTerm}</span> videos
+      </Typography>
+
+      <Videos videos={videos} />
+    </Box>
+  );
+};
+
+export default SearchFeed;
